24d26f32bdaaecf08c441236f7719f60a8337096
zelkourban <zelo.urban@gmail.com>
2020-06-09 10:41:59 +0000

    calc-fix

--- vypocet/vypocet.php
+++ vypocet/vypocet.php
@@ -9,15 +9,14 @@ include "../api_key.php";
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" type="text/css" href="../style.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
-    <script src="../script.js"></script>
-
+  
 
     <title><?php echo COUNT; ?></title>
 </head>
@@ -45,7 +44,7 @@ include "../api_key.php";
                         <?php echo MATHEMATICS; ?>
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <a class="dropdown-item" href="../vypocet/vypocet.php"><?php echo COUNT; ?> </a>
+                        <a class="dropdown-item" href="calc-api"><?php echo COUNT; ?> </a>
                         <a class="dropdown-item" href="../stats/stats.php">Statistics </a>
                     </div>
                 </li>
